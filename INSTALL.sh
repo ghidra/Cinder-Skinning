175565eff45e4b4720e003db9efd03f58bd057ac
ghidra <ghidra@gmail.com>
2018-11-18 23:23:38 -0500

    added assimp as submodule

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm -r $DIR/lib/
+#rm -r $DIR/lib/
 
-curl -O https://dl.dropboxusercontent.com/u/29102565/lib.zip && \
-unzip lib.zip && \
-rm lib.zip
+#curl -O https://dl.dropboxusercontent.com/u/29102565/lib.zip && \
+#unzip lib.zip && \
+#rm lib.zip
+
+cd $DIR/lib/assimp
+cmake -DCMAKE_CXX_FLAGS="-stdlib=libc++ -fvisibility-inlines-hidden" -DCMAKE_BUILD_TYPE=Release -DASSIMP_BUILD_STATIC_LIB=true
